c57fa5ce05375910ce44a24cf1bbe8f76406faaa
Pratik Dhoke <pratikdhoke13@gmail.com>
2019-10-02 19:28:11 +0530

    change

--- contact.php
+++ contact.php
@@ -1,2 +1,22 @@
 <?php
 
+if (isset($POST["submit"])){
+
+
+  $name = $_POST['name'];
+  $subject = $_POST['subject'];
+  $mailform = $_POST['mail'];
+  $message = $_POST['message'];
+  
+  $mailTo ="gameaddictx.info@gmail.com";
+   
+  $headers ="From: ".$mailform;
+
+  $txt ="You have received an e-mail from ".$name.".\n\n".$message;
+
+  mail($mailTo, $subject, $headers);
+
+  header("Location: index.php?mailsend")
+
+
+}
